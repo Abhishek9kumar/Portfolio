62b4d7b46e1ecad1b47b2a649a5ae88b010d6c79
Abhishek9kumar <abhishekkothiya97@gmail.com>
2025-08-16 01:10:03 +0530

    Add DW resume

--- src/components/Objective.js
+++ src/components/Objective.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFloppyDisk } from '@fortawesome/free-regular-svg-icons';
 
 export default function Objective(props) {
   return (
-    <section className="position-relative text-white py-5 px-4 overflow-hidden" style={{backgroundColor: "#000000"}}>
-        <div className="container position-relative z-1">
-        <p className="text-white-50" style={{ textAlign: "justify" }} data-aos="fade-up">
+    <section className="flex-d position-relative text-white py-5 px-4 overflow-hidden" style={{ backgroundColor: "#000000" }}>
+      <div className="container position-relative z-1" data-aos="fade-up">
+        <p className="text-white-50" style={{ textAlign: "justify" }}>
           {props.Objective}
         </p>
+        <div>
+          <a href="https://drive.google.com/uc?export=download&id=1lOTbO6e0kQbPkc0kkv2CmfQ7GaVLetAl" className="download_btn">
+            <FontAwesomeIcon icon={faFloppyDisk} bounce style={{ "--fa-animation-duration": "2s", paddingRight: "5px" }}
+            />
+            Download Resume
+          </a>
+        </div>
       </div>
     </section>
   )
